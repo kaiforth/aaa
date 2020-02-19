beb2e7e43964bf8c3cf1b24f6c80eda034d1e330
kai <1016062747@qq.com>
2020-02-19 17:51:11 +0800

    添加配置文件解决跨域问题

---
+++ src/main/java/xyz/godkai/cms/configs/CrossOrigin/CrossOriginConfig.java
@@ -0,0 +1,26 @@
+package xyz.godkai.cms.configs.CrossOrigin;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CrossOriginConfig {
+    private CorsConfiguration buildConfig() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.addAllowedOrigin("*");
+        return corsConfiguration;
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", buildConfig());
+        return new CorsFilter(urlBasedCorsConfigurationSource);
+    }
+
+}
